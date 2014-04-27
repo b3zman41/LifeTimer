a48c2607078127c1cd9620fa4881faade7b56ea8
Terence Bezman <terencebezman@gmail.com>
2014-04-27 00:54:49 -0400

    Init Commit

---
+++ Life Timer/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Life Timer
+//
+//  Created by Terence Bezman on 3/21/14.
+//  Copyright (c) 2014 Terence Bezman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
